3fe0df5cf3d5d2adf5b0d0c1fffa7d292d97cc92
puneetv22 <puneetv@iMac22.local>
2014-12-19 12:07:43 +0700

    Initial Commit

---
+++ 3MGCP Master/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  3MGCP Master
+//
+//  Created by puneetv22 on 12/19/2557 BE.
+//  Copyright (c) 2557 1105. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
